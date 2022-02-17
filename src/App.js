872b5e31f7c2f641661f6d5066e5170ea6cd8531
sistiri <sistiri@yahoo.com>
2022-02-17 12:27:34 +0100

    Wrapping the Transition Component

--- src/App.js
+++ src/App.js
@@ -48,19 +48,11 @@ class App extends Component {
                 transition: "opacity 1s ease-out",
                 opacity: state === "exiting" ? 0 : 1,
               }}
-            >
-              {" "}
-            </div>
+            ></div>
           )}
         </Transition>
-        <Transition
-          mountOnEnter
-          unmountOnExit
-          in={this.state.modalIsOpen}
-          timeout={300}
-        >
-          {(state) => <Modal show={state} closed={this.closeModal} />}
-        </Transition>
+
+          <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
 
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,23 +1,40 @@
 import React from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
-  const cssClasses = [
-    "Modal",
-    props.show === "entering"
-      ? "ModalOpen"
-      : props.show === "exiting"
-      ? "ModalClosed"
-      : null,
-  ];
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModelClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
