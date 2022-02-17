aa96705fd6915ab522db5b96cc6e03b32c43d178
sistiri <sistiri@yahoo.com>
2022-02-17 11:56:18 +0100

    Using ReactTransitionGroup - Animate => mountOnEnter, unmountOnExit

--- src/App.js
+++ src/App.js
@@ -33,19 +33,27 @@ class App extends Component {
         </button>
         <br />
         {/* {this.state.showBlock ? ( */}
-          <Transition in={this.state.showBlock} timeout={300}>
-            {state => <p>{state}</p>}
-            {/* <div
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
               style={{
                 backgroundColor: "red",
                 width: 100,
                 height: 100,
                 margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
               }}
             >
               {" "}
-            </div> */}
-          </Transition>
+            </div>
+          )}
+        </Transition>
         {/* // ) : null} */}
         {this.state.modalIsOpen ? (
           <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
